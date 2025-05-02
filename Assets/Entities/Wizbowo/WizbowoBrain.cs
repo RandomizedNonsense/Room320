e8b41bdac6aae67fde048d49bb20002166f46f73
Jacob Vanluven <46078617+KingToot14@users.noreply.github.com>
2025-05-02 09:35:13 -0400

    Added Wizbowo Animations + Prefab
    Added the basic idle, wave, and slam animations to Wizbowo. Also created a base for the prefab (sprite, rigidbody, collider, etc.)

---
+++ Assets/Entities/Wizbowo/WizbowoBrain.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class WizbowoBrain : MonoBehaviour
+{
+    private bool isAttacking = false;   // Determines whether or not Wizbowo is currenlty acting, or idling
+
+    [Range(0f, 10f, 0.5f)]
+    [SerializeField] float idleTime = 5f;   // How long Wizbowo idles before resuming attacks
+
+    [Range(0f, 1f, 0.05f)]
+    [SerializeField] float waveAttackOdds = 0.5f;   // The odds of choosing the "WandWave" attack over the "WandSlam" attack
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+
+    void WandWave() {
+        // Notes: in this attack, Wizbowo waves his wand around, spawning database projectiles towards the player
+
+    }
+
+    void WandSlam() {
+        // Notes: in this attack, Wizbowo slams his wand against the ground twice before disappearing, shortly reappearing with two or three duplicates
+    }
+}
